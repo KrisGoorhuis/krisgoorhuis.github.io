38ece414862498e388fd70edf95de293b7ebd76e
Kris <krisgoorhuis@gmail.com>
2016-10-14 17:39:33 -0700

    fixed a link, cutified a thing

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,19 +3,24 @@ document.addEventListener("DOMContentLoaded", function() {
    
    
    
-   window.onresize= checkWindowWidth;
+  
    
+   function testFunction() {
+      console.log("test success");
+   }
    
       
    function checkWindowWidth() {
       var windowWidth = (Math.max(document.documentElement.clientWidth));
       if (windowWidth < 1000) {
          document.getElementsByClassName("projectTile").style.width = "50%";
-         
+         console.log("success");
       }
    }
    
-   
+    window.onresize=testFunction() {
+       console.log("this");
+    };
 
 });
    
