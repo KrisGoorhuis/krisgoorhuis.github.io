fab7bd1ee3dd7ab1edeef06227de8d83834d5ca3
Kris Goorhuis <krisgoorhuis@gmail.com>
2017-05-30 10:01:18 -0700

    ...

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ var app = express();
 app.set('views', __dirname + '/app/views');
 app.engine('html', require('ejs').renderFile);
 //app.set('view engine', 'html');
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(process.env.PORT || 3000, function() { // Process.env.PORT is provided by Heroku (my host), I think. If it's not available (eg, testing locally) it'll default to 3000.
    console.log("Express started on port 3000");
+});
+
+app.get('/', function(request, response) {
+   response.sendfile(path.resolve(__dirname, 'public/index.html'));
 });
 
 app.get('/send', function(request, response) { // If a get request is sent to the base URL/send (which our contact button does), do this.
